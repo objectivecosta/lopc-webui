1cce255d5ac7b5d87adf5e9b077c9abf5476c9c5
Rafael Costa <rafael@rafaelcosta.me>
2016-06-22 18:09:13 -0300

    Started working on the webpages

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ var exphbs  = require('express-handlebars');
 global.app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 global.app.set('view engine', 'handlebars');
 
-//Defining middleware to serve static files
 global.app.use('/static', express.static('static'));
+
+app.get("/", function(req, res){
+  res.render("login")
+});
+
+global.app.listen(3001);
